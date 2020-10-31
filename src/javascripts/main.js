cd66ddd214e7ac4fffe4603731e4855a4be342bb
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2020-10-31 13:25:45 -0500

    made a random joke generator

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,24 @@
 import '../styles/main.scss';
+import axios from 'axios';
+import firebase from 'firebase/app';
+import firebaseKeys from './data/firebaseKeys.json';
+
+const baseUrl = firebaseKeys.databaseURL;
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
-};
+  $('#app').html('<h1>Joke Generator!</h1> ');
+  $('#joke').click(e => {
+    axios.get(`${baseUrl}/jokes.json`)
+    .then((response) => {
+      const jokeObject = response.data[Math.floor(Math.random() * response.data.length)]
+      const joke = jokeObject.joke
+      $('#viewJoke').replaceWith(`<p>${joke}</p> <button type="button" class="btn btn-secondary" id="getPunchline"> Show Me The Punchline</button>`);
+      $('#getPunchline').click(e => {
+        const punchline = jokeObject.punchline
+        $('#viewPunchline').replaceWith(`<p>${punchline}</p>`)
+      })
+    })
+  })
+  }
 
 init();
