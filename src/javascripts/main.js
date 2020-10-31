fb4d475b1e968b9849bf737908c8d5e67bc6e9a6
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2020-10-31 13:46:52 -0500

    fixed errors to deploy

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,24 +1,24 @@
 import '../styles/main.scss';
 import axios from 'axios';
-import firebase from 'firebase/app';
+// import firebase from 'firebase/app';
 import firebaseKeys from './data/firebaseKeys.json';
 
 const baseUrl = firebaseKeys.databaseURL;
 
 const init = () => {
   $('#app').html('<h1>Joke Generator!</h1> ');
-  $('#joke').click(e => {
-    axios.get(`${baseUrl}/jokes.json`)
-    .then((response) => {
-      const jokeObject = response.data[Math.floor(Math.random() * response.data.length)]
-      const joke = jokeObject.joke
-      $('#viewJoke').replaceWith(`<p>${joke}</p> <button type="button" class="btn btn-secondary" id="getPunchline"> Show Me The Punchline</button>`);
-      $('#getPunchline').click(e => {
-        const punchline = jokeObject.punchline
-        $('#viewPunchline').replaceWith(`<p>${punchline}</p>`)
-      })
-    })
-  })
-  }
+  $('#joke').click(() => {
+    axios.get(`${baseUrl}/jokes.json`).then((response) => {
+      const jokeObject = response.data[Math.floor(Math.random() * response.data.length)];
+      const { joke, punchline } = jokeObject;
+      $('#viewJoke').replaceWith(
+        `<p>${joke}</p> <button type="button" class="btn btn-secondary" id="getPunchline"> Show Me The Punchline</button>`
+      );
+      $('#getPunchline').click(() => {
+        $('#viewPunchline').replaceWith(`<p>${punchline}</p>`);
+      });
+    });
+  });
+};
 
 init();
